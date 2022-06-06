b18fbb90f382b960ee1ece91d97de1fdff584a8c
Its <2779546+Its-its@users.noreply.github.com>
2022-06-06 00:19:10 -0700

    Organized Edit Data Store

--- common/src/item/edit.rs
+++ common/src/item/edit.rs
@@ -1,10 +1,10 @@
 use chrono::{DateTime, Utc};
 use serde::{Serialize, Deserialize};
 
-use crate::{EditId, edit::*, util::*, TagId, PersonId, BookId, DisplayMetaItem, Member, MemberId, api::QueryListResponse, EditVoteId, ThumbnailStore};
+use crate::{EditId, edit::*, util::*, TagId, PersonId, BookId, Member, MemberId, api::QueryListResponse, EditVoteId, ThumbnailStore};
 
 
-
+pub use book_edit::*;
 
 
 
@@ -54,7 +54,6 @@ pub struct SharedEditVoteModel {
 }
 
 
-
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct UpdateEditModel {
 	pub status: Option<EditStatus>,
@@ -71,8 +70,6 @@ pub struct UpdateEditModel {
 
 
 
-// EditModel data field
-
 #[allow(clippy::large_enum_variant)]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum EditData {
@@ -83,102 +80,27 @@ pub enum EditData {
 }
 
 
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct BookEditData {
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub current: Option<DisplayMetaItem>,
-
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub new: Option<BookEdit>,
-
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub old: Option<BookEdit>, // Based off of current Model. If field is different than current Model once it's updating it'll ignore it.
-
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub updated: Option<UpdatedBookEdit>,
-}
-
-
-#[derive(Debug, Clone, Default, Serialize, Deserialize)]
-pub struct UpdatedBookEdit {
-	#[serde(skip_serializing_if = "is_false")]
-	pub title: bool,
-	#[serde(skip_serializing_if = "is_false")]
-	pub clean_title: bool,
-	#[serde(skip_serializing_if = "is_false")]
-	pub description: bool,
-
-	#[serde(skip_serializing_if = "is_false")]
-	pub rating: bool,
-
-	#[serde(skip_serializing_if = "is_false")]
-	pub isbn_10: bool,
-	#[serde(skip_serializing_if = "is_false")]
-	pub isbn_13: bool,
-
-	#[serde(skip_serializing_if = "is_false")]
-	pub is_public: bool,
-
-	#[serde(skip_serializing_if = "is_false")]
-	pub available_at: bool,
-	#[serde(skip_serializing_if = "is_false")]
-	pub language: bool,
-
-	#[serde(skip_serializing_if = "is_false")]
-	pub publisher: bool,
-}
-
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct BookEdit {
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub title: Option<String>,
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub clean_title: Option<String>,
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub description: Option<String>,
-
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub rating: Option<f64>,
-
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub isbn_10: Option<String>,
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub isbn_13: Option<String>,
-
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub is_public: Option<bool>,
-
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub available_at: Option<String>,
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub language: Option<u16>,
-
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub publisher: Option<String>,
-
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub added_people: Option<Vec<PersonId>>,
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub removed_people: Option<Vec<PersonId>>,
-
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub added_tags: Option<Vec<TagId>>,
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub removed_tags: Option<Vec<TagId>>,
-
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub added_images: Option<Vec<ThumbnailStore>>,
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub removed_images: Option<Vec<ThumbnailStore>>,
-}
-
 #[derive(Debug, Clone, Copy)]
 pub enum ModelIdGroup {
 	Book(BookId),
 	Person(PersonId),
 	Tag(TagId),
+}
+
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct InnerEditData<Curr, Cached, Update> {
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub current: Option<Curr>,
+
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub new: Option<Cached>,
+
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub old: Option<Cached>, // Based off of current Model. If field is different than current Model once it's updating it'll ignore it.
+
+	#[serde(skip_serializing_if = "Option::is_none")]
+	pub updated: Option<Update>,
 }
 
 
@@ -194,45 +116,124 @@ impl SharedEditModel {
 }
 
 
-
-impl UpdatedBookEdit {
-	pub fn is_empty(&self) -> bool {
-		!self.title &&
-		!self.clean_title &&
-		!self.description &&
-		!self.rating &&
-		!self.isbn_10 &&
-		!self.isbn_13 &&
-		!self.is_public &&
-		!self.available_at &&
-		!self.language &&
-		!self.publisher
-	}
-}
-
-
-
-impl BookEdit {
-	pub fn is_empty(&self) -> bool {
-		self.title.is_none() &&
-		self.clean_title.is_none() &&
-		self.description.is_none() &&
-		self.rating.is_none() &&
-		self.isbn_10.is_none() &&
-		self.isbn_13.is_none() &&
-		self.is_public.is_none() &&
-		self.available_at.is_none() &&
-		self.language.is_none() &&
-		self.publisher.is_none() &&
-		self.added_people.is_none() &&
-		self.removed_people.is_none() &&
-		self.added_tags.is_none() &&
-		self.removed_tags.is_none() &&
-		self.added_images.is_none() &&
-		self.removed_images.is_none()
-	}
-}
-
 fn is_false(value: &bool) -> bool {
 	!*value
+}
+
+
+mod book_edit {
+	use crate::DisplayMetaItem;
+	use super::*;
+
+	pub type BookEditData = InnerEditData<DisplayMetaItem, BookEdit, UpdatedBookEdit>;
+
+	#[derive(Debug, Clone, Serialize, Deserialize)]
+	pub struct BookEdit {
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub title: Option<String>,
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub clean_title: Option<String>,
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub description: Option<String>,
+
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub rating: Option<f64>,
+
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub isbn_10: Option<String>,
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub isbn_13: Option<String>,
+
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub is_public: Option<bool>,
+
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub available_at: Option<String>,
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub language: Option<u16>,
+
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub publisher: Option<String>,
+
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub added_people: Option<Vec<PersonId>>,
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub removed_people: Option<Vec<PersonId>>,
+
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub added_tags: Option<Vec<TagId>>,
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub removed_tags: Option<Vec<TagId>>,
+
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub added_images: Option<Vec<ThumbnailStore>>,
+		#[serde(skip_serializing_if = "Option::is_none")]
+		pub removed_images: Option<Vec<ThumbnailStore>>,
+	}
+
+	#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+	pub struct UpdatedBookEdit {
+		#[serde(skip_serializing_if = "is_false")]
+		pub title: bool,
+		#[serde(skip_serializing_if = "is_false")]
+		pub clean_title: bool,
+		#[serde(skip_serializing_if = "is_false")]
+		pub description: bool,
+
+		#[serde(skip_serializing_if = "is_false")]
+		pub rating: bool,
+
+		#[serde(skip_serializing_if = "is_false")]
+		pub isbn_10: bool,
+		#[serde(skip_serializing_if = "is_false")]
+		pub isbn_13: bool,
+
+		#[serde(skip_serializing_if = "is_false")]
+		pub is_public: bool,
+
+		#[serde(skip_serializing_if = "is_false")]
+		pub available_at: bool,
+		#[serde(skip_serializing_if = "is_false")]
+		pub language: bool,
+
+		#[serde(skip_serializing_if = "is_false")]
+		pub publisher: bool,
+	}
+
+
+	impl UpdatedBookEdit {
+		pub fn is_empty(&self) -> bool {
+			!self.title &&
+			!self.clean_title &&
+			!self.description &&
+			!self.rating &&
+			!self.isbn_10 &&
+			!self.isbn_13 &&
+			!self.is_public &&
+			!self.available_at &&
+			!self.language &&
+			!self.publisher
+		}
+	}
+
+	impl BookEdit {
+		pub fn is_empty(&self) -> bool {
+			self.title.is_none() &&
+			self.clean_title.is_none() &&
+			self.description.is_none() &&
+			self.rating.is_none() &&
+			self.isbn_10.is_none() &&
+			self.isbn_13.is_none() &&
+			self.is_public.is_none() &&
+			self.available_at.is_none() &&
+			self.language.is_none() &&
+			self.publisher.is_none() &&
+			self.added_people.is_none() &&
+			self.removed_people.is_none() &&
+			self.added_tags.is_none() &&
+			self.removed_tags.is_none() &&
+			self.added_images.is_none() &&
+			self.removed_images.is_none()
+		}
+	}
 }
